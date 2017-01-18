96737c33cded02df6781912f7e6c527857001037
Alimspender <akillcool@outlook.com>
2017-01-18 16:06:22 +0800

    Add Hash solution for 3. Longest Substring Without Repeating Characters

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -6,35 +6,24 @@
 //求最长非重复子串
 int lengthOfLongestSubstring(char* s)
 {
-	if (strlen(s) == 0)
-		return 0;
-
-	int maxLengh = 1;
-	int totalLength = strlen(s);
-	for (int i = 0; i < totalLength; ++i)
+	int i, j;
+	int maxLengh = 0;
+	int hash[256];
+	int n = strlen(s);
+	for (i = 0; i<n; ++i)
 	{
-		for (int j = i + 1; j < totalLength; ++j)
+		memset(hash, 0, sizeof(hash));
+		hash[s[i]] = 1;
+		for (j = i + 1; j<n; ++j)
 		{
-			int flag = 0;
-			for (int m = i; m <= j; ++m)
-			{
-				for (int n = m + 1; n <= j; ++n)
-				{
-					if (s[n] == s[m])
-					{
-						flag = 1;
-						break;
-					}
-				}
-				if (flag == 1)
-				{
-					break;
-				}
-			}
-			if (flag == 0 && j - i + 1 > maxLengh)
-			{
-				maxLengh = j - i + 1;
-			}
+			if (hash[s[j]] == 0)
+				hash[s[j]] = 1;
+			else
+				break;
+		}
+		if (j - i > maxLengh)
+		{
+			maxLengh = j - i;
 		}
 	}
 	return maxLengh;
