3496e860786c794308011fee1369d3fe4944481e
Alimspender <akillcool@outlook.com>
2017-01-19 12:02:31 +0800

    Add another solution for 3.Longest Substring Without Repeating Characters

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -1,37 +1,49 @@
 // leetcode.cpp
 
 #include "stdafx.h"
+#include <stdlib.h>
 #include <string.h>
 
 //求最长非重复子串
 int lengthOfLongestSubstring(char* s)
 {
-	int i, j;
 	int maxLengh = 0;
-	int hash[256];
+	//int begin = 0;
 	int n = strlen(s);
-	for (i = 0; i<n; ++i)
+	int * next = (int*)malloc(sizeof(int)*n); //next[i]记录了下一个与str[i]重复的字符的位置
+	int * first = (int*)malloc(sizeof(int)*(n + 1)); //first[i]记录str[i]后面最近的一个重复点
+	int hash[256];
+	memset(hash, 'n', sizeof(hash));		//memset一般只用于赋值成0或-1，这里使用是有问题的，但是并不影响结果
+
+	first[n] = n;
+	for (int i = n - 1; i >= 0; i--)
 	{
-		memset(hash, 0, sizeof(hash));
-		hash[s[i]] = 1;
-		for (j = i + 1; j<n; ++j)
+		next[i] = hash[s[i]];
+		hash[s[i]] = i;
+		if (next[i] < first[i + 1])
+			first[i] = next[i];
+		else
+			first[i] = first[i + 1]; //生成first[]表，复杂度是O(N)的
+		//printf("%d\t%d\n", i, first[i]);
+		//printf("%d\t%d\n", i, next[i]);
+	}
+	for (int i = 0; i<n; i++)
+	{
+		if (first[i] - i > maxLengh)
 		{
-			if (hash[s[j]] == 0)
-				hash[s[j]] = 1;
-			else
-				break;
-		}
-		if (j - i > maxLengh)
-		{
-			maxLengh = j - i;
+			maxLengh = first[i] - i;
+			//begin = i;
 		}
 	}
+	free(first);
+	free(next);
+	//printf("%.*s\n", maxLengh, &s[begin]);
 	return maxLengh;
 }
 
 int main(void)
 {
-	char targetString[8] = { "bbbbbbb" };
+	char targetString[8] = { "abcabcd" };
 	printf("%d\n", lengthOfLongestSubstring(targetString));
 }
 
